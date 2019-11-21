d5d1a43a269fc955ce765633f8ee66efda54585f
Hyunjin Bae <gloria9705@naver.com>
2019-11-21 14:01:11 +0900

    changed light.ino

--- light/light.ino
+++ light/light.ino
@@ -1,66 +1,113 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <SPI.h>
-#include <Wire.h>
 
 #define OLED_RESET LED_BUILTIN  //4
 Adafruit_SSD1306 display(OLED_RESET);
 
+int ledPin = D8;    // LED connected to digital pin 9
 int soundSensor = A0;     // 사운드 센서 핀
 int soundValue;
-int threshold = 68; // 큰 소리 크기 기준
-const int fade = 10; // delay time
-int led = D15;
+int threshold = 110; // 큰 소리 크기 기준
 
-void setup()
-{
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
-  display.display();
+void setup() {
+  // nothing happens in setup
+ display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+ display.clearDisplay();
+ display.display();
+ display.setTextSize(2);
+ display.setTextColor(WHITE);
+ display.setCursor(0,0);
 
  Serial.begin(9600); // 시리얼모니터 출력
  pinMode(soundSensor, INPUT);  // 사운드 센서 입력으로 설정  
 
-
 }
 
-void loop()
-{
+void loop() {
   soundValue = analogRead(soundSensor);
   Serial.println(soundValue);
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(1,1);
+  display.print("24H cafe ->");
+  display.display();
+  
+  analogWrite(ledPin, 255);
+  
+  // fade out from max to min in increments of 5 points:
+  if(soundValue >= threshold){
+    for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+      // sets the value (range from 0 to 255):
+      analogWrite(ledPin, fadeValue);
+      // wait for 30 milliseconds to see the dimming effect
+      delay(30);
+    }
+    analogWrite(ledPin, 0);
     display.setTextSize(1);
     display.setTextColor(WHITE);
     display.setCursor(1,1);
     display.clearDisplay();
-    display.print("24 Hour Cafe");
+    display.print("Please be Quiet.");
     display.display();
-  if(soundValue >= threshold){    // 감지된 소리의 크기가 500 이상일 때
-    draw();
-     for(int i=255; i>0; i--){ 
-        analogWrite(led, i); // LED 끄기
-        delay(50);
-      }
-
-     for(int i=0; i<255; i++){ 
-        analogWrite(led, i); // LED 켜기
-        delay(50);
-       }
+    display.clearDisplay();
+    
+    delay(6000);
   }
+}#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET LED_BUILTIN  //4
+Adafruit_SSD1306 display(OLED_RESET);
+
+int ledPin = D8;    // LED connected to digital pin 9
+int soundSensor = A0;     // 사운드 센서 핀
+int soundValue;
+int threshold = 110; // 큰 소리 크기 기준
+
+void setup() {
+  // nothing happens in setup
+ display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+ display.clearDisplay();
+ display.display();
+ display.setTextSize(2);
+ display.setTextColor(WHITE);
+ display.setCursor(0,0);
+
+ Serial.begin(9600); // 시리얼모니터 출력
+ pinMode(soundSensor, INPUT);  // 사운드 센서 입력으로 설정  
+
 }
 
-void draw(){
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();
+void loop() {
+  soundValue = analogRead(soundSensor);
+  Serial.println(soundValue);
+
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(1,1);
-  display.clearDisplay();
-  display.print("Please be Quiet.");
+  display.print("24H cafe ->");
   display.display();
-  delay(1000);
+  
+  analogWrite(ledPin, 255);
+  
+  // fade out from max to min in increments of 5 points:
+  if(soundValue >= threshold){
+    for (int fadeValue = 255 ; fadeValue >= 0; fadeValue -= 5) {
+      // sets the value (range from 0 to 255):
+      analogWrite(ledPin, fadeValue);
+      // wait for 30 milliseconds to see the dimming effect
+      delay(30);
+    }
+    analogWrite(ledPin, 0);
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(1,1);
+    display.clearDisplay();
+    display.print("Please be Quiet.");
+    display.display();
+    display.clearDisplay();
+    
+    delay(6000);
+  }
 }
-
-
- 
